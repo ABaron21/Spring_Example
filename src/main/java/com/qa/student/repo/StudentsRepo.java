31daa0289ef38d5aa09c0ed535b13c91b03701bc
Alex <abaron756@gmail.com>
2022-03-03 12:21:19 +0000

    Student Repo created

--- src/main/java/com/qa/student/repo/StudentsRepo.java
+++ src/main/java/com/qa/student/repo/StudentsRepo.java
@@ -1,9 +1,11 @@
 package com.qa.student.repo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.qa.student.domain.Students;
 
+@Repository
 public interface StudentsRepo extends JpaRepository<Students, Long> {
 
 }
