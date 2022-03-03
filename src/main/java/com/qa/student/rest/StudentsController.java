06bf5e2c7f818087c73b5636ecebebe7ee087357
Alex <abaron756@gmail.com>
2022-03-03 12:31:29 +0000

    Create method made

--- src/main/java/com/qa/student/rest/StudentsController.java
+++ src/main/java/com/qa/student/rest/StudentsController.java
@@ -1,5 +1,25 @@
 package com.qa.student.rest;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.qa.student.domain.Students;
+import com.qa.student.service.StudentsService;
+
+@RestController
 public class StudentsController {
 
+	private StudentsService service;
+
+	public StudentsController(StudentsService service) {
+		super();
+		this.service = service;
+	}
+
+	// create - post request
+	@PostMapping("/create")
+	public Students createMap(@RequestBody Students info) {
+		return this.service.createNew(info);
+	}
 }
--- src/main/java/com/qa/student/service/StudentsService.java
+++ src/main/java/com/qa/student/service/StudentsService.java
@@ -1,5 +1,23 @@
 package com.qa.student.service;
 
+import org.springframework.stereotype.Service;
+
+import com.qa.student.domain.Students;
+import com.qa.student.repo.StudentsRepo;
+
+@Service
 public class StudentsService {
+
+	private StudentsRepo repo;
+
+	public StudentsService(StudentsRepo repo) {
+		super();
+		this.repo = repo;
+	}
+
+	// create
+	public Students createNew(Students info) {
+		return this.repo.save(info);
+	}
 
 }
