d09f67352da45dbf1d4d4308d272d1d2f8fe802b
Alex <abaron756@gmail.com>
2022-03-04 09:43:43 +0000

    Basic Controller completed

--- src/main/java/com/qa/student/service/StudentsService.java
+++ src/main/java/com/qa/student/service/StudentsService.java
@@ -1,6 +1,7 @@
 package com.qa.student.service;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
@@ -25,5 +26,28 @@ public class StudentsService {
 	// ReadAll
 	public List<Students> readAll() {
 		return this.repo.findAll();
+	}
+
+	// Read By Id
+	public Students readId(Long id) {
+		Optional<Students> optionalStudent = this.repo.findById(id);
+		if (optionalStudent.isPresent()) {
+			return optionalStudent.get();
+		}
+		return null;
+	}
+
+	// Update
+	public Students updateInfo(Long id, Students newInfo) {
+		Students oldInfo = this.repo.getById(id);
+		oldInfo.setName(newInfo.getName());
+		oldInfo.setAge(newInfo.getAge());
+		oldInfo.setPhoneNumber(newInfo.getPhoneNumber());
+		return this.repo.save(oldInfo);
+	}
+
+	// Delete
+	public void delete(Long id) {
+		this.repo.deleteById(id);
 	}
 }
